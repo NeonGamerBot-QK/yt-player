d2e2f20a9d033d77694083173df12dc37e6d1862
Saahil <saahilattud@gmail.com>
2022-12-08 16:34:06 -0500

    useEffect errors and better debug

--- pages/index.js
+++ pages/index.js
@@ -13,17 +13,17 @@ useEffect(() => {
 const url = process.browser ? new URLSearchParams( window.location) : null;
 console.debug("URL STATE", url)
 if(url) {
-  console.debug("QUERY LOADED", url.values())
   const playlist_url = url.get("p");
   const song_index = url.get("s");
   const autoload = url.get("a");
+  console.debug("QUERY LOADED", url.values(), playlist_url, songIndex, autoload)
 if(songs.length === 0) {
 updateUrl(playlist_url);
 setSongIndex(songIndex)
 if(autoload) onLoadPlaylist();
 }
 }
-})
+}, [songs.length, songIndex, onLoadPlaylist])
   const [songs, setSongs] = useState([]);
   const [info, setInfo] = useState({});
   const [isplaying, setisplaying] = useState(false);
@@ -75,7 +75,7 @@ if(url) {
     else {
       audioElem.current.pause();
     }
-  }, [currentSong, isplaying, songs])
+  }, [currentSong, isplaying, songs, songIndex])
 const onLoadPlaylist = async () => {
   const playlist_d = await fetch("/api/search?url="+url).then(r=>r.json())
   console.log(playlist_d)
