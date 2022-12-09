dc4fb512d9e33be01596b993d6f6ef67051ddc5a
Saahil <saahilattud@gmail.com>
2022-12-09 15:56:48 -0500

    atpment 1

--- pages/api/download.js
+++ pages/api/download.js
@@ -6,6 +6,7 @@ import {FormData} from "formdata-node"
 const url_regex = /^(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/
 export default async function handler(req, res) {
 const url = req.query.url
+const streamData = []
 console.debug("URL DOWNLOAD ", url, url_regex.test(url))
     if(req.method !== "GET") return res.status(405).json({ error: "Invalid method, only 'GET' is allowed" })
  if(!url) return res.status(400).json({ error: "No URL supplied, supply one by using the ?url= parameter" });
@@ -19,7 +20,7 @@ if(!url_regex.test(url)) return res.status(400).json({ error: "The url is invali
  stream.once('response', () => {
    starttime = Date.now();
  });
- stream.pipe(res)
+ //stream.pipe(res)
 stream.on("progress",  (chunkLength, downloaded, total) => {
    const percent = downloaded / total;
    const downloadedMinutes = (Date.now() - starttime) / 1000 / 60;
@@ -29,11 +30,12 @@ console.log(`running for: ${downloadedMinutes.toFixed(2)}minutes, estimated time
   })
  stream.on("end", () => {
    console.log("STREAM END")
-res.end();
+res.end(Buffer.contact(streamData));
 })
-// stream.on("data", (chunk) => {
-//    console.log("DATA", chunk)
-// }) 
+stream.on("data", (chunk) => {
+   console.log("DATA", chunk)
+streamData.push(chunk)
+}) 
 //  const fdata = new FormData()
 //   fdata.append("file", stream)
 //  fetch("http://n1.saahild.com:2014/upload", {
