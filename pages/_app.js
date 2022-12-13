a036c2f95675f638732d02ccedd610cede5b14eb
neon <neon@saahild.com>
2022-12-14 00:14:31 +0100

    e

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import '../styles/globals.css';
 import "../styles/player.scss";
-import "@madzadev/audio-player/dist/index.css";
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
