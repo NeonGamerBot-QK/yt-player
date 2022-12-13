b675346e35163b96e3a96824faf3f5304c53efe8
neon <neon@saahild.com>
2022-12-14 00:08:36 +0100

    u

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
-const withImages = require("next-images");
-const withTM = require("next-transpile-modules")(["@madzadev/audio-player"]);
-const extra_config = withImages(withTM())
+//const withImages = require("next-images");
+//const withTM = require("next-transpile-modules")(["@madzadev/audio-player"]);
+//const extra_config = withImages(withTM())
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  ...withImages(withTM())
+ 
 }
 // console.log(extra_config, new String(extra_config.webpack).toString())
-module.exports = extra_config;
+module.exports = nextConfig;
